7d014f4a6c791460db6c3e1885156a22018e8f20
senura <senu.lakshan@gmail.com>
2025-06-12 15:52:46 +0530

    unit test update

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5174', // Point to your app’s local server
-    specPattern: 'cypress/e2e/**/*.spec.js', // Pattern for test files
+    baseUrl: 'http://localhost:5174',
+    specPattern: 'cypress/e2e/**/*.spec.js',
     supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
       // Optional: Add plugins or custom tasks here
